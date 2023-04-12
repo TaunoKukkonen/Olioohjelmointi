afbc509503e37c0f72a8eec7efa96548c234a019
Tauno Kukkonen <taunokukkonen@hotmail.fi>
2023-04-12 14:22:13 +0300

    Pilli tehty

---
+++ Whisle/Whisle/Pilli.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whisle
+{
+    internal class Pilli
+    {
+        private String _aani;
+
+        public Pilli(String pillinAani)
+            {
+                this._aani = pillinAani;
+
+            }
+
+        public void soi()
+        {
+            Console.WriteLine(_aani);  
+        }
+
+
+    }
+
+}
---
+++ Whisle/Whisle/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Whisle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilli sorsapilli = new Pilli("Kvaak");
+            Pilli kukkopilli = new Pilli("Peef");
+            sorsapilli.soi();
+            kukkopilli.soi();
+            sorsapilli.soi();
+        }
+    }
+}
