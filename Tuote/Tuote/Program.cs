5921137a123228d23d037931606d0c68623659b4
Tauno Kukkonen <taunokukkonen@hotmail.fi>
2023-04-17 11:00:34 +0300

    tuote tehty

---
+++ Tuote/Tuote/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Tuote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tuote banaani = new Tuote("banaani", 1.1, 13);
+            banaani.TulostaTuote();
+        }
+    }
+}
---
+++ Tuote/Tuote/Tuote.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuote
+{
+    internal class Tuote
+    {
+        private string nimiAlussa;
+        private double hintaAlussa;
+        private int maaraAlussa;
+
+
+        public Tuote(string nimiAlussa, double hintaAlussa, int maaraAlussa)
+        {
+            this.nimiAlussa = nimiAlussa;
+            this.hintaAlussa = hintaAlussa;
+            this.maaraAlussa = maaraAlussa;
+        }
+
+        public void TulostaTuote()
+        {
+            Console.WriteLine(nimiAlussa+"hinta " + hintaAlussa+" "+ maaraAlussa+ " kpl" );
+        }
+
+
+    }
+}
