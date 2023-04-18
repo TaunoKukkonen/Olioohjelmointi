ec219bc766344de6c7b96daea91d4730a750b0c8
Tauno Kukkonen <taunokukkonen@hotmail.fi>
2023-04-18 14:39:25 +0300

    Mitta valmis

---
+++ pituus/pituus/Mittari.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pituus
+{
+    internal class Mittari
+    {
+        private int _mitta;
+
+        public Mittari() 
+        {
+            _mitta = 0;
+        }
+
+        public void Lisaa()
+        {
+            _mitta++;
+        }
+        public void Vahenna()
+        {
+            _mitta--;
+        }
+        public int Mitta()
+        { return _mitta; }
+        public Boolean Taynna()
+        { return _mitta == 5; }
+
+
+    }
+}
---
+++ pituus/pituus/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace pituus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mittari m = new Mittari();
+            while (!m.Taynna())
+            {
+                Console.WriteLine("Ei täynnä! Mitta: " + m.Mitta());
+                m.Lisaa();
+            }
+            Console.WriteLine("Täynnä! Mitta: " + m.Mitta());
+            m.Vahenna();
+            Console.WriteLine("Ei täynnä! Mitta: " + m.Mitta());
+        }
+    }
+}
