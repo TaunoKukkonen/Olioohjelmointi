4df33d3e0e6c8cfe393bc7d8a2a0bafbd7121a73
Tauno Kukkonen <taunokukkonen@hotmail.fi>
2023-04-18 15:53:18 +0300

    maksukortti tehty

---
+++ Maksukortti/maksukortti/Maksukortti.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paaohjelma
+{
+    public class Maksukortti
+    {
+        private double saldo;
+        public Maksukortti(double alkusaldo)
+        {
+            this.saldo = alkusaldo;
+        }
+        public override string ToString()
+        {
+            return "Kortilla on rahaa "+this.saldo+ " Euroa";
+
+        }
+        public void syoEdullisesti()
+        {   if (this.saldo > 2.60) 
+            { this.saldo = saldo - 2.60; }
+        }
+        public void syoMaukkaasti()
+        {
+            if (this.saldo > 4.60)
+            { this.saldo = saldo - 4.60; }
+        }
+        public void LataaRahaa(int talletus)
+        { if (talletus > 0)
+            { this.saldo += talletus; }
+        }
+    }
+}
---
+++ Maksukortti/maksukortti/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace paaohjelma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maksukortti Pekka =new Maksukortti(20);
+            Maksukortti Matti = new Maksukortti(30);
+            Pekka.syoMaukkaasti();
+            Matti.syoEdullisesti();
+            Console.WriteLine("Pekkan "+ Pekka);
+            Console.WriteLine("Mattin " + Matti);
+            Pekka.LataaRahaa(20);
+            Matti.syoMaukkaasti();
+            Console.WriteLine("Pekkan " + Pekka);
+            Console.WriteLine("Mattin " + Matti);
+            Pekka.syoEdullisesti();
+            Pekka.syoEdullisesti();
+            Matti.LataaRahaa(50);
+            Console.WriteLine("Pekkan " + Pekka);
+            Console.WriteLine("Mattin " + Matti);
+
+        }
+    }
+}
