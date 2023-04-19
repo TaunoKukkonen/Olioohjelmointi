59d2b82753684740f52cc2da2d2fb386e08e8446
Tauno Kukkonen <taunokukkonen@hotmail.fi>
2023-04-19 14:57:58 +0300

    periytyminen tehty

---
+++ Periytyminen/Program.cs
@@ -0,0 +1,30 @@
+﻿using ElainLuokat;
+
+namespace Periytyminen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kissa newkissa = new Kissa();
+            newkissa.AsetaElaimenNimi("Mauno");
+            newkissa.AsetaElaimenIka(8);
+            newkissa.AsetaOnLihanSyoja(true);
+            newkissa.kehrata();
+            Console.WriteLine(newkissa.PalautaElaimenNimi() + " " + newkissa.PalautaElaimenIka() + " " + newkissa.palautaOnLihanSyoja());
+
+            Koira newKoira = new Koira();
+            newKoira.AsetaElaimenNimi("Danny");
+            newKoira.AsetaElaimenIka(15);
+            newKoira.AsetaOnLihanSyoja(true);
+            Console.WriteLine(newKoira.PalautaElaimenNimi()+ " "+ newKoira.PalautaElaimenIka()+" "+ newKoira.palautaOnLihanSyoja());
+            newKoira.Aantele();
+
+            
+
+            
+            newkissa.Aantele();
+            newKoira.Aantele();
+        }
+    }
+}
